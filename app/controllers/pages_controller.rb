f29c4156ab2fa06fee06f10ec738177a1395b2da
Christian Lawaczeck <christian.lawaczeck@gmail.com>
2010-10-26 18:55:07 +0200

    Erstmal Ende mit statischen Seiten

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @title = 'home'
   end
 
   def contact
+    @title = 'contact'
+  end
+  
+  def list
+    @title = 'list'
+  end
+  
+  def about
+    @title = 'Über uns'
   end
 
 end
