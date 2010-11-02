f034cfeceb56f01ebb09dc2b836209d8cb609426
Christian Lawaczeck <christian.lawaczeck@gmail.com>
2010-11-02 12:46:21 +0100

    Removed default index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,12 @@ require 'User'
 class PagesController < ApplicationController
   def home
     @title = 'home'
-    @user = User.new(:name => 'c', :email => 'c')
+#    @user = User.new(:name => 'c', :email => 'c')
   end
 
   def contact
     @title = 'contact'
-    @user = User.new(:name => 'c', :email => 'c')
+#    @user = User.new(:name => 'c', :email => 'c')
   end
   
   def list
